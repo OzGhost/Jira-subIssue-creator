699e952701d4409681afe3f89877a42206649aa6
nhduc <duc.nguyenhoang@axonactive.com>
2019-08-20 10:26:07 +0700

    Configurator injected

--- jsc.js
+++ jsc.js
@@ -1,6 +1,102 @@
 'use strict';
 (function(){
-    var app = function() {
+    var Configurator = function(iCollection, submitCallback) {
+        let _self = this;
+        _self.iCollection = iCollection || [];
+        _self.shelf = undefined;
+        _self.cage = undefined;
+        _self.submitCallback = submitCallback;
+        _self.frame = ''
+                +'<div class="configurator">'
+                    +'<div class="front">'
+                        +'<div class="list">'
+                            +'<p>Subtask name(s)</p>'
+                            +'<ol id="stn">'
+                            +'</ol>'
+                        +'</div>'
+                        +'<div class="ctrl">'
+                            +'<p>Your input, sir</p>'
+                            +'<form id="input-form"><input type="text" placeholder="Subtask\'s name..." /></form>'
+                            +'<button id="submit" class="primitive">Save</button>'
+                            +'<button id="cancel">Cancel</button>'
+                        +'</div>'
+                    +'</div>'
+                +'</div>';
+
+        _self.install = function() {
+            _self.cage = document.createElement("div");
+            _self.cage.innerHTML = _self.frame;
+            document.body.appendChild(_self.cage);
+
+            _self.shelf = _self.cage.querySelector("#stn");
+            if ( ! _self.shelf) return;
+            let len = _self.iCollection.length;
+            for (let i = 0; i < len; ++i) {
+                _self.putToShelf(_self.iCollection[i]);
+            }
+
+            let fr = _self.cage.querySelector("form#input-form");
+            if ( ! fr) return;
+            let ip = fr.querySelector("input");
+            if ( ! ip) return;
+            fr.addEventListener("submit", function(e){
+                e.preventDefault();
+                let val = ip.value;
+                _self.add( val );
+                ip.value = "";
+            });
+
+            let submitTrigger = _self.cage.querySelector("#submit");
+            if ( ! submitTrigger) return;
+            submitTrigger.addEventListener("click", function(){
+                _self.submitCallback && _self.submitCallback(_self.iCollection);
+            });
+
+            let cancelTrigger = _self.cage.querySelector("#cancel");
+            if ( ! cancelTrigger) return;
+            cancelTrigger.addEventListener("click", function(){
+                document.body.removeChild(_self.cage);
+            });
+        };
+
+        _self.putToShelf = function(val) {
+            let li = document.createElement("li");
+            li.innerHTML = val + " <span>&times;</span>"
+            li.className = "isubtask";
+            _self.shelf.appendChild(li);
+
+            let trigger = li.querySelector("span");
+            if ( ! trigger) return;
+            trigger.addEventListener("click", function(e) {
+                _self.remove( val, e.target );
+            });
+        };
+
+        _self.remove = function(val, el) {
+            let cc = _self.iCollection;
+            _self.iCollection = [];
+            let len = cc.length;
+            for (let i = 0; i < len; ++i) {
+                if (cc[i].toUpperCase() !== val.toUpperCase()) {
+                    _self.iCollection.push(cc[i]);
+                }
+            }
+            _self.shelf.removeChild(el.parentNode);
+        };
+
+        _self.add = function(val) {
+            let valTrim = val.trim();
+            let len = _self.iCollection.length;
+            for (let i = 0; i < len; ++i) {
+                if (valTrim.toUpperCase() === _self.iCollection[i].toUpperCase())
+                    return;
+            }
+            _self.iCollection.push(valTrim);
+            _self.putToShelf(val);
+        };
+    };
+
+    var JSC = function() {
         var _self = this;
         _self.ports = [];
         _self.taskNames = [];
@@ -163,23 +259,23 @@
 
         _self.shift = function(projectKey, issueKey, taskName, lightHook) {
             fetch("https://jira.axonivy.com/jira/rest/api/2/issue/", {
-                    method: "POST",
-                    headers: {
-                        "Content-Type": "application/json"
-                    },
-                    body: _self.createPayloadToAddIssue(projectKey, issueKey, taskName)
+                method: "POST",
+                headers: {
+                    "Content-Type": "application/json"
+                },
+                body: _self.createPayloadToAddIssue(projectKey, issueKey, taskName)
             })
-            .then(function(rs){
-                if (rs.ok) {
-                    _self.insertSuccessDot(lightHook);
-                } else {
+                .then(function(rs){
+                    if (rs.ok) {
+                        _self.insertSuccessDot(lightHook);
+                    } else {
+                        _self.insertFailureDot(lightHook);
+                    }
+                })
+                .catch(function(error){
+                    console.error(error);
                     _self.insertFailureDot(lightHook);
-                }
-            })
-            .catch(function(error){
-                console.error(error);
-                _self.insertFailureDot(lightHook);
-            });
+                });
         };
 
         _self.createPayloadToAddIssue = function(projectKey, issueKey, taskName) {
@@ -223,18 +319,19 @@
         };
 
         _self.openConfigurator = function() {
-            let configurator = document.createElement("div");
-            configurator.className = "configurator";
-            document.body.appendChild(configurator);
+            new Configurator(_self.taskNames, function(iCollection){
+                chrome.storage.local.set({"cfg": iCollection}, function() {
+                    window.location.reload();
+                });
+            }).install();
         };
-
     };
 
     // load stored configuration
     chrome.storage.local.get(["cfg"], function(storage) {
         var rawCfg = storage.cfg || {};
-        var myApp = new app();
-        myApp.setTasksToCreate(["Sonar", "Crosscheck"]);
+        var myApp = new JSC();
+        myApp.setTasksToCreate(rawCfg);
         myApp.watch();
     });
 
